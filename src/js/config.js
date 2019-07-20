c37a90a848f650a9d6d0dfd8659606e42e600563
manzoku <su.soft.support@gmail.com>
2019-07-20 20:49:50 +0900

    フォーム設計情報取得kintoneAPIの廃止に伴い、フィールド取得とレイアウト取得のkintoneAPIに変更

--- src/js/config.js
+++ src/js/config.js
@@ -5,23 +5,31 @@ jQuery.noConflict();
 
 	$(function() {
 
-		kintone.api(kintone.api.url('/k/v1/form', true), 'GET', {
-			'app': kintone.app.getId()
-		}, function(resp) {
+		kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET', {'app': kintone.app.getId()})
+		.then(function(resp) {
 			$.each(resp['properties'], function(index, property) {
+				if (property['type'] === 'SUBTABLE' || property['type'] === 'CATEGORY' || property['type'] === 'STATUS') {
+					return;
+				}
 				$('#target-column').append('<option value="' + property['code'] + '">' + property['label'] + '</option>');
 			});
 			var config = kintone.plugin.app.getConfig(pluginId);
 			if (config['target']) {
 				$('#target-column').val(config['target']);
 			}
+
+			$('#setting-submit').click(function() {
+				var config = {};
+				config['target'] = $('#target-column').val();
+				kintone.plugin.app.setConfig(config);
+			});
+		}, function(fieldsResp) {
+			alert('フォーム情報の取得ができませんでした。');
+			$('#setting-submit').on('click', function() {
+				alert('フォームデータ取得ができなかったため保存できません。\nキャンセルしてください。');
+			});
 		});
 
-		$('#setting-submit').click(function() {
-			var config = {};
-			config['target'] = $('#target-column').val();
-			kintone.plugin.app.setConfig(config);
-		});
 		$('#setting-cancel').on('click', function() {
 			history.back();
 		});
